8ba64d18cac4da94bfbf70928c9624c0619d2b7e
PranayB003 <pranayborgohain@gmail.com>
2023-07-11 21:59:28 +0530

    added: vscode settings and keybindings

--- utils/setup_app_preferences.sh
+++ utils/setup_app_preferences.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo $TERTIARY copying app preferences...
 cp ./Library/Preferences/* ~/Library/Preferences/
+
+# vscode
+echo $TERTIARY setting up VSCode settings and keybindings...
+cp ./Library/Application\ Support/Code/User/*.json ~/Library/Application\ Support/Code/User/
