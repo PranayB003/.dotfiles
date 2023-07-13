795650de2a8073cbe65ccef53192e6542a247010
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-07-13 19:34:05 +0530

    modified: tab change keybindings for nvim. Only changed syntax, hehaviour remains unchanged.

--- config/.config/nvim/lua/core/keybindings.lua
+++ config/.config/nvim/lua/core/keybindings.lua
@@ -4,5 +4,5 @@ vim.keymap.set("n", "<Leader>tt", ":NvimTreeFindFileToggle<CR>")
 vim.keymap.set("n", "<Leader>tf", ":NvimTreeOpen | NvimTreeFocus<CR>")
 
 -- move to the next/previous tab
-vim.keymap.set("n", "<Esc><Tab>", ":tabnext<CR>")
-vim.keymap.set("n", "<Esc><S-Tab>", ":tabprevious<CR>")
+vim.keymap.set("n", "<M-Tab>", ":tabnext<CR>")
+vim.keymap.set("n", "<M-S-Tab>", ":tabprevious<CR>")
