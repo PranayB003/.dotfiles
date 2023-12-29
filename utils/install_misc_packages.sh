a75fadfbcd7ba0dfc874a772134fab23eb686be3
Pranay <66739544+PranayB003@users.noreply.github.com>
2023-12-29 03:58:18 +0000

    updated: tpm install location, .bashrc, .zshrc

--- utils/install_misc_packages.sh
+++ utils/install_misc_packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install package manager for tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+# Automatically install tmux plugins instead of having to press prefix+I on first launch
+~/.config/tmux/plugins/tpm/bin/install_plugins
 
 # Install plugins for vim
 git clone git@github.com:christoomey/vim-tmux-navigator.git ~/.vim/pack/plugins/start/vim-tmux-navigator
--- utils/reconfigure_locales.sh
+++ utils/reconfigure_locales.sh
@@ -4,7 +4,6 @@
 # https://mkyong.com/linux/cannot-set-lc_ctype-to-default-locale-no-such-file-or-directory/
 # https://askubuntu.com/questions/599808/cannot-set-lc-ctype-to-default-locale-no-such-file-or-directory
 sudo locale-gen "en_US.UTF-8"
-sudo dpkg-  reconfigure locales
 
 # test whether it worked
 locale
