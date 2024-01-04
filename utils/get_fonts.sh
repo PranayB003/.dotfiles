5fef36a450662e6cc1e1b354beee870cb3a6284e
Pranay <66739544+PranayB003@users.noreply.github.com>
2024-01-05 02:26:23 +0530

    fixed: install_fonts.sh, create fonts directory if not present

--- utils/get_fonts.sh
+++ utils/get_fonts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.tar.xz
+sudo mkdir -p /usr/local/share/fonts
 sudo tar -C /usr/local/share/fonts -xvf Hack.tar.xz
 cd ~/.dotfiles
 rm Hack.tar.xz
