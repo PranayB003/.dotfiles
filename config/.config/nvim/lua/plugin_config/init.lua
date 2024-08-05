d62a5bb0c45400a46f085a9e6ac4e0e6eefb5d4f
Pranay <66739544+PranayB003@users.noreply.github.com>
2024-08-06 01:57:56 +0530

    added: gruvbox theme to nvim and tmux, dracula theme to starship

--- config/.config/nvim/lua/plugin_config/init.lua
+++ config/.config/nvim/lua/plugin_config/init.lua
@@ -3,3 +3,5 @@ require("plugin_config.telescope")
 require("plugin_config.nvim-tree")
 require("plugin_config.treesitter")
 require("plugin_config.better-vim-tmux-resizer")
+require("plugin_config.gruvbox")
+
